8c5d62aa804c59caa814362fdee1f5fb6fec2e57
ZhaonanLi <zhaonanproject@gmail.com>
2016-08-17 20:09:03 -0700

    Initial Commit

---
+++ ios-camera-video-save-to-file-frames-AVAssetWriterInputPixelBufferAdaptor/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ios-camera-video-save-to-file-frames-AVAssetWriterInputPixelBufferAdaptor
+//
+//  Created by Zhaonan Li on 8/17/16.
+//  Copyright © 2016 Zhaonan Li. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
